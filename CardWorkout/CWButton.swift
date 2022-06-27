fc508f48a7d75689d068064fc2b6ca9aa83e2b48
Olzh <@>
2022-06-27 19:45:57 +0600

    First commit, complete app

---
+++ CardWorkout/CWButton.swift
@@ -0,0 +1,35 @@
+//
+//  CWButton.swift
+//  CardWorkout
+//
+//  Created by MAC on 22.06.2022.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+    //Custom for button (name convention for custom elements - CWButton)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero) //origin and size for zero
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+        configure()
+    }
+    
+    func configure() {
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
